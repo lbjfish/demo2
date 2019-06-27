5c8537257c9d6ceb87a0aab0994fbfd0bc015f90
libaojun <lbj20190614ht>
2019-06-27 10:28:24 +0800

    加一下注解的标识

--- module/content-module/src/main/java/com/lee/content/ContentApplication.java
+++ module/content-module/src/main/java/com/lee/content/ContentApplication.java
@@ -3,7 +3,7 @@ package com.lee.content;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+//EnableEurekaClient表明为EurekaClient
 @EnableEurekaClient
 @SpringBootApplication
 public class ContentApplication {
