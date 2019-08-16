3f7502c8ac83b6173c966b9e84a0234eebe12864
libaojun <lbj20190614ht>
2019-08-16 18:18:50 +0800

    gateway之类的

--- module/gateway-module/src/main/java/com/lee/gateway/GatewayApplication.java
+++ module/gateway-module/src/main/java/com/lee/gateway/GatewayApplication.java
@@ -1,14 +1,24 @@
 package com.lee.gateway;
 
+import com.lee.gateway.filter.TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+//通过@EnableDiscoveryClient向服务中心发现（这样就能调用别的被添加到eureka的服务,如果需要服务间调用，就必加）
+@EnableDiscoveryClient
+//EnableEurekaClient表明为EurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 @RestController
 public class GatewayApplication {
@@ -28,12 +38,42 @@ public class GatewayApplication {
                                         .setName("mycmd")
                                         .setFallbackUri("forward://fallback")))
                         .uri("http://httpbin.org:80"))
+                .route(p -> p.cookie("name","abcc").uri("http://httpbin.org/delay/3"))
                 .build();
     }
 
-    @RequestMapping("/fallback/ppp")
+    @GetMapping("/fallback/ppp")
     public Mono<String> fallback() {
         return Mono.just("fallback");
     }
 
+    /***************************现在3个Bean可以放到一个类里，带@Configuration的***************************/
+    //IP限流
+    //@Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+    }
+
+    //用户限流
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("userId"));
+    }
+
+    //接口限流
+    //@Bean
+    KeyResolver apiKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getPath().value());
+    }
+    /***************************上面3个Bean可以放到一个类里，带@Configuration的***************************/
+
+
+//    /**
+//     * 全局自定义过滤器 （我先关了，要不我访问任何地址都会被拦截）
+//     * @return
+//     */
+//    @Bean
+//    public TokenFilter tokenFilter(){
+//        return new TokenFilter();
+//    }
 }
---
+++ module/gateway-module/src/main/java/com/lee/gateway/controller/TestController.java
@@ -0,0 +1,37 @@
+package com.lee.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/defaultfallback")
+    public Map<String,String> defaultfallback(){
+        System.out.println("降级操作...");
+        Map<String,String> map = new HashMap<>();
+        map.put("resultCode","fail");
+        map.put("resultMessage","服务异常");
+        map.put("resultObj","null");
+        return map;
+    }
+
+    @RequestMapping("/hello")
+    public String hello(String name){
+        return "hi " + name;
+    }
+
+    @RequestMapping("/timeout")
+    public String timeout(){
+        try{
+            //睡5秒，网关Hystrix3秒超时，会触发熔断降级操作
+            Thread.sleep(5000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "timeout2";
+    }
+}
