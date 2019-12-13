e7ef0fa50cc66c7d0e029e5b2e1c9a939f156d98
libj <libj@akulaku.com>
2019-12-13 17:52:54 +0800

    feign + 文件下载公能

--- module/feignweb-module/src/main/java/com/lee/feignweb/client/ContentClient.java
+++ module/feignweb-module/src/main/java/com/lee/feignweb/client/ContentClient.java
@@ -1,6 +1,7 @@
 package com.lee.feignweb.client;
 
 import com.lee.feignweb.controller.feign.MyTemporaryPo;
+import feign.Response;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
