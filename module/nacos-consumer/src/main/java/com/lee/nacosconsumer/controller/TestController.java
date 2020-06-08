19d364f95c3e35393378a1b20de87465582d8072
lbj <libaojun@dhgate.com>
2020-06-08 20:14:12 +0800

    nacos配置中心+多环境下“管理”及“隔离”配置和服务+共享配置

--- module/nacos-consumer/src/main/java/com/lee/nacosconsumer/controller/TestController.java
+++ module/nacos-consumer/src/main/java/com/lee/nacosconsumer/controller/TestController.java
@@ -2,14 +2,15 @@ package com.lee.nacosconsumer.controller;
 
 import com.lee.nacosconsumer.client.NacosProviderClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
-
-
+@RefreshScope
 public class TestController {
     @Autowired
     private NacosProviderClient providerClient;
@@ -17,6 +18,59 @@ public class TestController {
     @GetMapping(value = "/echo/{str}")
     public String testabc(@PathVariable String str){
         return providerClient.getEcho(str);
+    }
+
+
+    /************************************ 测试 naocs 配置中心 ************************************/
+
+    @Value("${zhang:zzz}")
+    private String zhang;
+
+    @GetMapping("/getConfig")
+    public String getNacosConfig() {
+        return zhang;
+    }
+
+    @Value("${nacos.share}")
+    private String share;
+
+
+    @Value("${share.config1}")
+    private String shareConfig1;
+
+    @Value("${share.config2}")
+    private String shareConfig2;
+
+    @RequestMapping("/getValue")
+    public String getValue() {
+        return share;
+    }
+
+    @RequestMapping("/getShare1")
+    public String getShare1() {
+        return shareConfig1;
+    }
+
+    @RequestMapping("/getShare2")
+    public String getShare2() {
+        return shareConfig2;
+    }
+
+
+    @Value("${share.config3}")
+    private String shareConfig3;
+
+    @Value("${share.config4}")
+    private String shareConfig4;
+
+    @RequestMapping("/getShare3")
+    public String getShare3() {
+        return shareConfig3;
+    }
+
+    @RequestMapping("/getShare4")
+    public String getShare4() {
+        return shareConfig4;
     }
 
 }
--- module/nacos-provider/src/main/java/com/lee/nacosprovider/controller/EchoController.java
+++ module/nacos-provider/src/main/java/com/lee/nacosprovider/controller/EchoController.java
@@ -1,11 +1,70 @@
 package com.lee.nacosprovider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RefreshScope
 public class EchoController {
+    /************************************ 测试 naocs 注册中心 ************************************/
     @GetMapping(value = "/echo/{string}")
     public String echo(@PathVariable String string) {
         return "Hello Nacos Discovery " + string;
+    }
+
+
+
+
+    /************************************ 测试 naocs 配置中心 ************************************/
+
+    @Value("${zhang:zzz}")
+    private String zhang;
+
+    @GetMapping("/getConfig")
+    public String getNacosConfig() {
+        return zhang;
+    }
+
+    @Value("${nacos.share}")
+    private String share;
+
+
+    @Value("${share.config1}")
+    private String shareConfig1;
+
+    @Value("${share.config2}")
+    private String shareConfig2;
+
+    @RequestMapping("/getValue")
+    public String getValue() {
+        return share;
+    }
+
+    @RequestMapping("/getShare1")
+    public String getShare1() {
+        return shareConfig1;
+    }
+
+    @RequestMapping("/getShare2")
+    public String getShare2() {
+        return shareConfig2;
+    }
+
+
+    @Value("${share.config3}")
+    private String shareConfig3;
+
+    @Value("${share.config4}")
+    private String shareConfig4;
+
+    @RequestMapping("/getShare3")
+    public String getShare3() {
+        return shareConfig3;
+    }
+
+    @RequestMapping("/getShare4")
+    public String getShare4() {
+        return shareConfig4;
     }
 }
