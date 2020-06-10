0f7036ff9377969e370eda421e04aaed4388d742
lbj <libaojun@dhgate.com>
2020-06-10 15:29:22 +0800

    nacos 换maven版本 com.alibaba的

--- module/nacos-consumer/src/main/java/com/lee/nacosconsumer/controller/TestController.java
+++ module/nacos-consumer/src/main/java/com/lee/nacosconsumer/controller/TestController.java
@@ -35,10 +35,10 @@ public class TestController {
     private String share;
 
 
-    @Value("${share.config1}")
-    private String shareConfig1;
+//    @Value("${share.config1}")
+//    private String shareConfig1;
 
-    @Value("${share.config2}")
+    @Value("${share.config0}")
     private String shareConfig2;
 
     @RequestMapping("/getValue")
@@ -46,10 +46,10 @@ public class TestController {
         return share;
     }
 
-    @RequestMapping("/getShare1")
-    public String getShare1() {
-        return shareConfig1;
-    }
+//    @RequestMapping("/getShare1")
+//    public String getShare1() {
+//        return shareConfig1;
+//    }
 
     @RequestMapping("/getShare2")
     public String getShare2() {
@@ -57,16 +57,16 @@ public class TestController {
     }
 
 
-    @Value("${share.config3}")
-    private String shareConfig3;
+//    @Value("${share.config3}")
+//    private String shareConfig3;
 
-    @Value("${share.config4}")
+    @Value("${share.config}")
     private String shareConfig4;
 
-    @RequestMapping("/getShare3")
-    public String getShare3() {
-        return shareConfig3;
-    }
+//    @RequestMapping("/getShare3")
+//    public String getShare3() {
+//        return shareConfig3;
+//    }
 
     @RequestMapping("/getShare4")
     public String getShare4() {
