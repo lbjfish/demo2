597bb250ed40c3b0c90c4cba1bb91be0bd2e217e
lbj <libaojun@dhgate.com>
2020-10-20 17:49:14 +0800

    看VersionIsolationRule->详细用法，可以学会TransmittableThreadLocal，然后实现ribbon选择我想要的实例，可以过滤测试环境只走测试实例，过滤其他人注册的实例

--- module/feignweb-module/src/main/java/com/lee/feignweb/ribbonconfig/rule/GlobalRule.java
+++ module/feignweb-module/src/main/java/com/lee/feignweb/ribbonconfig/rule/GlobalRule.java
@@ -2,12 +2,20 @@ package com.lee.feignweb.ribbonconfig.rule;
 
 import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.AbstractLoadBalancerRule;
+import com.netflix.loadbalancer.BaseLoadBalancer;
+import com.netflix.loadbalancer.ILoadBalancer;
 import com.netflix.loadbalancer.Server;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class GlobalRule extends AbstractLoadBalancerRule {
 
+    private AtomicInteger nextServerCyclicCounter;
+
+    public GlobalRule() {
+        this.nextServerCyclicCounter = new AtomicInteger(0);
+    }
 
     @Override
     public void initWithNiwsConfig(IClientConfig iClientConfig) {
@@ -16,10 +24,26 @@ public class GlobalRule extends AbstractLoadBalancerRule {
 
     @Override
     public Server choose(Object o) {
+        BaseLoadBalancer balancer = (BaseLoadBalancer) getLoadBalancer();
+        String b = balancer.getName();
         List<Server> upList = this.getLoadBalancer().getReachableServers();
         List<Server> servers = this.getLoadBalancer().getAllServers();
         System.out.println(upList);
         System.out.println(servers);
-        return servers.get(0);
+
+        int nextServerIndex = incrementAndGetModulo(servers.size());
+        return servers.get(nextServerIndex);
     }
+
+    private int incrementAndGetModulo(int modulo) {
+        int current;
+        int next;
+        do {
+            current = this.nextServerCyclicCounter.get();
+            next = (current + 1) % modulo;
+        } while(!this.nextServerCyclicCounter.compareAndSet(current, next));
+
+        return next;
+    }
+
 }
---
+++ module/feignweb-module/src/main/java/com/lee/feignweb/ribbonconfig/rule/VersionIsolationRule.java
@@ -0,0 +1,79 @@
+package com.lee.feignweb.ribbonconfig.rule;
+
+import org.springframework.cloud.alibaba.nacos.ribbon.NacosServer;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.RoundRobinRule;
+import com.netflix.loadbalancer.Server;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 根据实例版本号隔离规则
+ * 详细用法：https://mp.weixin.qq.com/s/9XQ-SIbYsov3KBx9TGFN0g
+ *
+ * 实例获取规则顺序如下(不满足则走下一个规则)：
+ * 1. 相同版本号的实例
+ * 2. 无版本号的实例
+ * 3. 所有实例中轮询
+ *
+ * @author zlt
+ * @date 2019/9/3
+ * <p>
+ * Blog: https://zlt2000.gitee.io
+ * Github: https://github.com/zlt2000
+ */
+public class VersionIsolationRule extends RoundRobinRule {
+    private final static String KEY_DEFAULT = "default";
+    /**
+     * 优先根据版本号取实例
+     */
+    @Override
+    public Server choose(ILoadBalancer lb, Object key) {
+        if (lb == null) {
+            return null;
+        }
+        String version;
+        if (key != null && !KEY_DEFAULT.equals(key)) {
+            version = key.toString();
+        } else {
+            version = "aaa";
+        }
+
+        List<Server> targetList = null;
+        List<Server> upList = lb.getReachableServers();
+        if (!StringUtils.isEmpty(version)) {
+            //取指定版本号的实例
+            targetList = upList.stream().filter(
+                    server -> version.equals(
+                            ((NacosServer) server).getMetadata().get("version")
+                    )
+            ).collect(Collectors.toList());
+        }
+
+        if (CollectionUtils.isEmpty(targetList)) {
+            //只取无版本号的实例
+            targetList = upList.stream().filter(
+                    server -> {
+                        String metadataVersion = ((NacosServer) server).getMetadata().get("versio");
+                        return StringUtils.isEmpty(metadataVersion);
+                    }
+            ).collect(Collectors.toList());
+        }
+
+        if (!CollectionUtils.isEmpty(targetList)) {
+            return getServer(targetList);
+        }
+        return super.choose(lb, key);
+    }
+
+    /**
+     * 随机取一个实例
+     */
+    private Server getServer(List<Server> upList) {
+//        int nextInt = RandomUtil.randomInt(upList.size());
+        return upList.get(0);
+    }
+}
